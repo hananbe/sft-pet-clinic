54e870d6fe592a6cc809f4acf8766e283659071c
ber.hanan <ber.hanan@gmail.com>
2019-10-01 16:10:55 +0300

    adding base entity.

---
+++ pet-clinic-data/src/main/java/com/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.guru.springframework.sfgpetclinic.model;
+import java.io.Serializable;
+
+/**
+ * Created by jt on 7/18/18.
+ */
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/com/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,4 @@
 package com.guru.springframework.sfgpetclinic.model;
 
-public class Owner {
+public class Owner extends Person{
 }
