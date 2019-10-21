251d2fa37be84b3126b71a340bc2b53b3ae02a5d
ber.hanan <ber.hanan@gmail.com>
2019-10-21 14:33:50 +0300

    Add project lombok. closes #51

--- pet-clinic-data/src/main/java/com/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,5 +1,8 @@
 package com.guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,17 +12,17 @@ import java.io.Serializable;
 /**
  * Created by jt on 7/18/18.
  */
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
+    public BaseEntity(Long id) {
+        this.id=id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public BaseEntity() {
     }
 }
