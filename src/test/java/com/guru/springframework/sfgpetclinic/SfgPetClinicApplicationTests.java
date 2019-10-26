d3fcd92362963d8230baaf1947e70a8b47055ada
ber.hanan <ber.hanan@gmail.com>
2019-10-26 17:59:56 +0300

    upgrade to JUnit 5 . closes #4

--- src/test/java/com/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/com/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,16 +1,19 @@
 package com.guru.springframework.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 public class SfgPetClinicApplicationTests {
 
     @Test
     public void contextLoads() {
+    
     }
+
 
 }
